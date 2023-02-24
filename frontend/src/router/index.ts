2e33b5084dea69c96857462cf1dd9d928b41e1ff
ismail <ismail@bazookka.com>
2023-02-24 15:57:59 -0500

    set route to freidnlist

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import FriendListView from '../views/FriendListView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/friendList',
+    name: 'friendlist',
+    component: FriendListView,
   },
   {
     path: '/about',
