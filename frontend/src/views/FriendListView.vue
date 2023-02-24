65fa742675f76c148919fcdad0ddd4f1129ab623
ismail <ismail@bazookka.com>
2023-02-24 15:48:32 -0500

    set view for friendlist

---
+++ frontend/src/views/FriendListView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="friendList">
+        <h1>FriendList</h1>
+        <FriendList/>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import FriendList from '@/components/FriendList.vue';
+
+export default defineComponent({
+  name: 'FriendListView',
+  components: {
+    FriendList,
+  },
+});
+</script>
