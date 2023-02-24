f55c799fa389da95d41b22d352880fc39d125de8
ismail <ismail@bazookka.com>
2023-02-24 15:47:23 -0500

    set cors on server

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,18 @@
 import express, {Express, Request, Response} from 'express'
+import cors, { CorsOptions } from 'cors'
 
 
 const app: Express  = express()
 
+const corsOptions: CorsOptions = {
+    origin: 'http://localhost:8080'
+}
+
+
 const friendsRoutes:any = require('./routes/friends')
 const commonArtistRoutes: any = require('./routes/common_artist')
 
+app.use(cors(corsOptions))
 app.use('/common_artist', commonArtistRoutes);
 app.use('/friends', friendsRoutes);
 
@@ -13,7 +20,13 @@ app.get('/', (req: Request,res : Response) => {
     res.send("hello world")
 })
 
-const PORT = process.env.PORT || 9001
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
+const PORT = process.env.PORT || 9002
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`)   
 })
